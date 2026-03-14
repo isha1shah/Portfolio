780abb67ee2fb7c03661c77be95cb28426b011a6
Isha shah <isha.shah2312@gmail.com>
2026-03-14 21:48:22 +0530

    Change resume link in About component
    Updated the resume link to a new Google Drive URL.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1hTd2vqzxfcRSU_UbfmJknvkhFH55zHeC/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1iBPBm43NtMrJCZwZsgHG9k80Gb-LIJg3/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
